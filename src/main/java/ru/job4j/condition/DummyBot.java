24a565aeb442ab35231dbe3072352570179d8b3d
ElizavetaKr <122363218+ElizavetaKr@users.noreply.github.com>
2024-01-17 14:12:21 +0300

    4.1.2. Глупый бот [#31323 #473462]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -1,15 +1,15 @@
 package ru.job4j.condition;
 
 public class DummyBot {
-public static String answer(String question) {
-    String result = "I don't know, Please, ask another question.";
-    if ("Hi, Bot.".equals(question)) {
-        result = "Hi, SmartAss.";
-    } else  if ("Bye.".equals(question)) {
-        result = "See you later.";
+    public static String answer(String question) {
+        String result = "I don't know, Please, ask another question.";
+        if ("Hi, Bot.".equals(question)) {
+            result = "Hi, SmartAss.";
+        } else if ("Bye.".equals(question)) {
+            result = "See you later.";
+        }
+        return result;
     }
-    return result;
-}
 
     public static void main(String[] args) {
         String result = DummyBot.answer("Hi, Bot.");
