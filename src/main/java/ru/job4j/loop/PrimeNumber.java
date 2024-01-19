559bebba74e9c1ea68c0b07c3abad84e7ee21e68
ElizavetaKr <122363218+ElizavetaKr@users.noreply.github.com>
2024-01-19 10:10:31 +0300

    4.5. Отладка программы в IDEA [#159464 #474433]

--- src/main/java/ru/job4j/loop/PrimeNumber.java
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class PrimeNumber {
     public static int calc(int finish) {
         int counter = 0;
-        for (int number = 2; number <= finish; number++) {
+        for (int number = 2; finish >= number; number++) {
             if (CheckPrimeNumber.check(number)) {
                 counter++;
             }
--- src/main/java/ru/job4j/loop/Slash.java
+++ src/main/java/ru/job4j/loop/Slash.java
@@ -2,8 +2,8 @@ package ru.job4j.loop;
 
 public class Slash {
     public static void draw(int size) {
-        for (int row = 0; row < size; row++) {
-            for (int cell = 0; cell < size; cell++) {
+        for (int row = 0; size > row; row++) {
+            for (int cell = 0; size > cell; cell++) {
                 boolean left = row + cell == size - 1;
                 boolean right = row == cell;
                 if (left) {
